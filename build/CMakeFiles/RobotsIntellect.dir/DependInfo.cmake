37fb8335fc46fb82eda930f5c0576d7ae7cf7941
Michal.Nowicki <Michal.Nowicki@38ae7965-9959-4cb9-b587-4855e4c9c4a6>
2013-07-07 23:05:42 +0000

    Dodalem troche kodu do zrzucania, ale mam problem z biblioteka urg pod arch linuxa. Sprawdze pod ubuntu.

---
+++ build/CMakeFiles/RobotsIntellect.dir/DependInfo.cmake
@@ -0,0 +1,56 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/home/smi/RobotosIntellect13/trunk/PositionEstimation/GPS/src/context.c" "/home/smi/RobotosIntellect13/trunk/build/CMakeFiles/RobotsIntellect.dir/PositionEstimation/GPS/src/context.c.o"
+  "/home/smi/RobotosIntellect13/trunk/PositionEstimation/GPS/src/generate.c" "/home/smi/RobotosIntellect13/trunk/build/CMakeFiles/RobotsIntellect.dir/PositionEstimation/GPS/src/generate.c.o"
+  "/home/smi/RobotosIntellect13/trunk/PositionEstimation/GPS/src/generator.c" "/home/smi/RobotosIntellect13/trunk/build/CMakeFiles/RobotsIntellect.dir/PositionEstimation/GPS/src/generator.c.o"
+  "/home/smi/RobotosIntellect13/trunk/PositionEstimation/GPS/src/gmath.c" "/home/smi/RobotosIntellect13/trunk/build/CMakeFiles/RobotsIntellect.dir/PositionEstimation/GPS/src/gmath.c.o"
+  "/home/smi/RobotosIntellect13/trunk/PositionEstimation/GPS/src/info.c" "/home/smi/RobotosIntellect13/trunk/build/CMakeFiles/RobotsIntellect.dir/PositionEstimation/GPS/src/info.c.o"
+  "/home/smi/RobotosIntellect13/trunk/PositionEstimation/GPS/src/parse.c" "/home/smi/RobotosIntellect13/trunk/build/CMakeFiles/RobotsIntellect.dir/PositionEstimation/GPS/src/parse.c.o"
+  "/home/smi/RobotosIntellect13/trunk/PositionEstimation/GPS/src/parser.c" "/home/smi/RobotosIntellect13/trunk/build/CMakeFiles/RobotsIntellect.dir/PositionEstimation/GPS/src/parser.c.o"
+  "/home/smi/RobotosIntellect13/trunk/PositionEstimation/GPS/src/sentence.c" "/home/smi/RobotosIntellect13/trunk/build/CMakeFiles/RobotsIntellect.dir/PositionEstimation/GPS/src/sentence.c.o"
+  "/home/smi/RobotosIntellect13/trunk/PositionEstimation/GPS/src/time.c" "/home/smi/RobotosIntellect13/trunk/build/CMakeFiles/RobotsIntellect.dir/PositionEstimation/GPS/src/time.c.o"
+  "/home/smi/RobotosIntellect13/trunk/PositionEstimation/GPS/src/tok.c" "/home/smi/RobotosIntellect13/trunk/build/CMakeFiles/RobotsIntellect.dir/PositionEstimation/GPS/src/tok.c.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/smi/RobotosIntellect13/trunk/MechanicalGrabber/MechanicalGrabber.cpp" "/home/smi/RobotosIntellect13/trunk/build/CMakeFiles/RobotsIntellect.dir/MechanicalGrabber/MechanicalGrabber.cpp.o"
+  "/home/smi/RobotosIntellect13/trunk/MovementConstraints/Hokuyo/Hokuyo.cpp" "/home/smi/RobotosIntellect13/trunk/build/CMakeFiles/RobotsIntellect.dir/MovementConstraints/Hokuyo/Hokuyo.cpp.o"
+  "/home/smi/RobotosIntellect13/trunk/MovementConstraints/MovementConstraints.cpp" "/home/smi/RobotosIntellect13/trunk/build/CMakeFiles/RobotsIntellect.dir/MovementConstraints/MovementConstraints.cpp.o"
+  "/home/smi/RobotosIntellect13/trunk/MovementConstraints/Sharp/Sharp.cpp" "/home/smi/RobotosIntellect13/trunk/build/CMakeFiles/RobotsIntellect.dir/MovementConstraints/Sharp/Sharp.cpp.o"
+  "/home/smi/RobotosIntellect13/trunk/Planning/GlobalPlanner.cpp" "/home/smi/RobotosIntellect13/trunk/build/CMakeFiles/RobotsIntellect.dir/Planning/GlobalPlanner.cpp.o"
+  "/home/smi/RobotosIntellect13/trunk/Planning/PlanningSearching/LocalPlannerSearching.cpp" "/home/smi/RobotosIntellect13/trunk/build/CMakeFiles/RobotsIntellect.dir/Planning/PlanningSearching/LocalPlannerSearching.cpp.o"
+  "/home/smi/RobotosIntellect13/trunk/Planning/PlanningToGold/LocalPlannerToGold.cpp" "/home/smi/RobotosIntellect13/trunk/build/CMakeFiles/RobotsIntellect.dir/Planning/PlanningToGold/LocalPlannerToGold.cpp.o"
+  "/home/smi/RobotosIntellect13/trunk/Planning/PlanningWithGold/LocalPlannerWithGold.cpp" "/home/smi/RobotosIntellect13/trunk/build/CMakeFiles/RobotsIntellect.dir/Planning/PlanningWithGold/LocalPlannerWithGold.cpp.o"
+  "/home/smi/RobotosIntellect13/trunk/PositionEstimation/Encoders/Encoders.cpp" "/home/smi/RobotosIntellect13/trunk/build/CMakeFiles/RobotsIntellect.dir/PositionEstimation/Encoders/Encoders.cpp.o"
+  "/home/smi/RobotosIntellect13/trunk/PositionEstimation/GPS/GPS.cpp" "/home/smi/RobotosIntellect13/trunk/build/CMakeFiles/RobotsIntellect.dir/PositionEstimation/GPS/GPS.cpp.o"
+  "/home/smi/RobotosIntellect13/trunk/PositionEstimation/IMU/IMU.cpp" "/home/smi/RobotosIntellect13/trunk/build/CMakeFiles/RobotsIntellect.dir/PositionEstimation/IMU/IMU.cpp.o"
+  "/home/smi/RobotosIntellect13/trunk/PositionEstimation/PositionEstimation.cpp" "/home/smi/RobotosIntellect13/trunk/build/CMakeFiles/RobotsIntellect.dir/PositionEstimation/PositionEstimation.cpp.o"
+  "/home/smi/RobotosIntellect13/trunk/Robot/Robot.cpp" "/home/smi/RobotosIntellect13/trunk/build/CMakeFiles/RobotsIntellect.dir/Robot/Robot.cpp.o"
+  "/home/smi/RobotosIntellect13/trunk/Trobot/source/Controller.cpp" "/home/smi/RobotosIntellect13/trunk/build/CMakeFiles/RobotsIntellect.dir/Trobot/source/Controller.cpp.o"
+  "/home/smi/RobotosIntellect13/trunk/Trobot/source/Gps.cpp" "/home/smi/RobotosIntellect13/trunk/build/CMakeFiles/RobotsIntellect.dir/Trobot/source/Gps.cpp.o"
+  "/home/smi/RobotosIntellect13/trunk/Trobot/source/Imu.cpp" "/home/smi/RobotosIntellect13/trunk/build/CMakeFiles/RobotsIntellect.dir/Trobot/source/Imu.cpp.o"
+  "/home/smi/RobotosIntellect13/trunk/Trobot/source/Motor.cpp" "/home/smi/RobotosIntellect13/trunk/build/CMakeFiles/RobotsIntellect.dir/Trobot/source/Motor.cpp.o"
+  "/home/smi/RobotosIntellect13/trunk/Trobot/source/RobotDrive.cpp" "/home/smi/RobotosIntellect13/trunk/build/CMakeFiles/RobotsIntellect.dir/Trobot/source/RobotDrive.cpp.o"
+  "/home/smi/RobotosIntellect13/trunk/Trobot/source/RoboteqDevice.cpp" "/home/smi/RobotosIntellect13/trunk/build/CMakeFiles/RobotsIntellect.dir/Trobot/source/RoboteqDevice.cpp.o"
+  "/home/smi/RobotosIntellect13/trunk/Trobot/source/SerialPort.cpp" "/home/smi/RobotosIntellect13/trunk/build/CMakeFiles/RobotsIntellect.dir/Trobot/source/SerialPort.cpp.o"
+  "/home/smi/RobotosIntellect13/trunk/Trobot/source/rangeSensor.cpp" "/home/smi/RobotosIntellect13/trunk/build/CMakeFiles/RobotsIntellect.dir/Trobot/source/rangeSensor.cpp.o"
+  "/home/smi/RobotosIntellect13/trunk/main.cpp" "/home/smi/RobotosIntellect13/trunk/build/CMakeFiles/RobotsIntellect.dir/main.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/include/opencv"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <stdio.h>
+#include <fstream>
+#include <unistd.h>
 
 #include "Robot/Robot.h"
 
@@ -10,6 +12,51 @@ int main()
 	Robot *ourRobot  = new Robot();
 
 
+	// Streams to save data
+	ofstream encoderStream, imuStream, gpsStream;
+	encoderStream.open("encoder.data");
+	imuStream.open("imu.data");
+	gpsStream.open("gps.data");'
+
+	// Opening to capture data
+	// Hardcoded devices -> ADJUST IT !
+	Robot->openGps("/dev/ttyS0");
+	Robot->openImu("/dev/ttyS1");
+	Robot->openRobotsDrive("/dev/ttyS2");
+
+	// Reading data
+	if ( Robot->isImuOpen() && Robot->isGpsOpen() )
+	{
+		cv::Mat encoderData, gpsData, imuData;
+		while(1)
+		{
+			encoderData = Robot->getEncoderData();
+			gpsData = Robot->getGpsData();
+			imuData = Robot->getImuData();
+
+			// enc data 2x1
+			encoderStream << encoderData[0]<< " "<<encoderData[1] << endl;
+
+			// gps data 2x1
+			gpsStream << gpsData[0] << " " << gpsData[1] << endl;
+
+			// I changed dimensions to be row-aligned -> each row represents data from 1 sensor
+			// imu data 4x3
+			for (int i = 0; i < 4; i++)
+				for (int j=0; j<3; j++)
+					imuStream << imuData[i][j] << " ";
+			imuStream << endl;
+
+			// Waiting 50 ms to have some rational data stream catching -> max 20 Hz
+			usleep(50 * 1000);
+
+		}
+	}
+
+	// Closing streams
+	encoderStream.close();
+	gpsStream.close();
+	imuStream.close();
 
 	delete ourRobot;
 }
