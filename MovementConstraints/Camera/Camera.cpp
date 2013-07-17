3d5f2a7c619864119f3746a09f4b6c87574cba30
jachu51 <jachu51@38ae7965-9959-4cb9-b587-4855e4c9c4a6>
2013-07-17 11:59:51 +0000

    Rozpocząłem prace nad rzutowaniem sklasyfikowanego obrazu na mapę.

--- MovementConstraints/Camera/Camera.cpp
+++ MovementConstraints/Camera/Camera.cpp
@@ -6,7 +6,13 @@
  */
 
 #include <opencv2/opencv.hpp>
+#include <cmath>
 #include "Camera.h"
+
+#define CAMERA_Z 1
+#define CAMERA_X_ANGLE 45
+#define CAMERA_Y_ANGLE 45
+#define CAMERAS_COUNT 2
 
 using namespace cv;
 using namespace std;
@@ -17,6 +23,65 @@ Camera::Camera(MovementConstraints* imovementConstraints) : movementConstraints(
 
 Camera::~Camera(){
 
+}
+
+void Camera::computeConstraints(std::vector<cv::Mat> image){
+	for(int im = 0; im < CAMERAS_COUNT; im++){
+		int rows = image[im].rows;
+		int cols = image[im].cols;
+		Mat cornersX(rows + 1, cols + 1, CV_32SC1);
+		Mat cornersY(rows + 1, cols + 1, CV_32SC1);
+		for(int nrow = 0; nrow < rows; nrow++){
+			for(int ncol = 0; ncol < cols; ncol++){
+				//computing top left corners
+				Point3f point = computePointProjection(	Point2f((float)(ncol - cols/2) / (cols/2),
+																-(float)(nrow - rows/2) / (rows/2)),
+														im);
+				cornersX.at<int>(nrow, ncol) = point.x;
+				cornersY.at<int>(nrow, ncol) = point.y;
+			}
+		}
+		for(int ncol = 0; ncol < cols; ncol++){
+			//computing bottom left corners
+			Point3f point = computePointProjection(	Point2f((float)(ncol - cols/2) / (cols/2),
+															-1),
+													im);
+			cornersX.at<int>(rows, ncol) = point.x;
+			cornersY.at<int>(rows, ncol) = point.y;
+		}
+		for(int nrow = 0; nrow < rows; nrow++){
+			//computing top right corners
+			Point3f point = computePointProjection(	Point2f(1,
+															-(float)(nrow - rows/2) / (rows/2)),
+													im);
+			cornersX.at<int>(nrow, cols) = point.x;
+			cornersY.at<int>(nrow, cols) = point.y;
+		}
+		//computing bottom right corner
+		Point3f point = computePointProjection(	Point2f(1,
+														-1),
+												im);
+		cornersX.at<int>(rows, cols) = point.x;
+		cornersY.at<int>(rows, cols) = point.y;
+		//TODO Rysowanie trapezów na mapie
+	}
+}
+
+cv::Point3f Camera::computePointProjection(cv::Point2f imPoint, int cameraInd){
+	Mat point(3, 1, CV_32FC1);
+	point.at<float>(0) = imPoint.x * CAMERA_Z * tan(CAMERA_X_ANGLE/2);
+	point.at<float>(1) = imPoint.y * CAMERA_Z * tan(CAMERA_Y_ANGLE/2);
+	point.at<float>(2) = -CAMERA_Z;
+
+	Mat rot(cameraOrig[cameraInd], Rect(0, 0, 2, 2));
+	Mat trans(cameraOrig[cameraInd], Rect(0, 3, 2, 3));
+	point = rot * point;
+	Mat planeABC(groundPlane, Rect(0, 0, 2, 0));
+	Mat planeD(groundPlane, Rect(3, 0, 3, 0));
+	Mat a = (-planeABC.t() * trans - planeD) / (planeABC.t() * point);
+	point = trans + point * a;
+	Point3f ret(point.at<float>(0), point.at<float>(1), point.at<float>(2));
+	return ret;
 }
 
 //CV_8UC3 2x640x480: left, right image
--- MovementConstraints/Camera/Camera.h
+++ MovementConstraints/Camera/Camera.h
@@ -8,12 +8,30 @@
 #ifndef CAMERA_H_
 #define CAMERA_H_
 
+#include <vector>
 #include <opencv2/opencv.hpp>
 
 class MovementConstraints;
 
 class Camera {
+	//Parent MovementConstraints class
 	MovementConstraints* movementConstraints;
+
+	//CV_32FC1 MAP_SIZExMAP_SIZE: 0-1 chance of being occupied, robot's position (MAP_SIZE/2, 0)
+	cv::Mat constraints;
+
+	//CV_32FC1 4x4: camera origin position and orientation
+	cv::Mat cameraOrig[2];
+
+	//CV_32FC1 4x1: ground plane equation [A, B, C, D]'
+	cv::Mat groundPlane;
+
+	//Grid of image classification
+	int cameraGrid;
+
+	void computeConstraints(std::vector<cv::Mat> image);
+
+	cv::Point3f computePointProjection(cv::Point2f imPoint, int cameraInd);
 public:
 	Camera(MovementConstraints* imovementConstraints);
 	virtual ~Camera();
