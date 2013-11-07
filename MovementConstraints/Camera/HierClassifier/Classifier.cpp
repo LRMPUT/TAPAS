fe7928f3ca690d4e8b157e971e2e537d84e177d6
jachu51 <jachu51@38ae7965-9959-4cb9-b587-4855e4c9c4a6>
2013-11-07 11:00:25 +0000

    Brakujący plik.

---
+++ MovementConstraints/Camera/HierClassifier/Classifier.cpp
@@ -0,0 +1,23 @@
+/*
+ * Classifier.cpp
+ *
+ *  Created on: 07-11-2013
+ *      Author: jachu
+ */
+
+#include "Classifier.h"
+
+Classifier::Classifier(){
+
+}
+
+/** \brief Loads settings from XML structure.
+
+*/
+Classifier::Classifier(TiXmlElement* settings){
+
+}
+
+Classifier::~Classifier(){
+
+}
