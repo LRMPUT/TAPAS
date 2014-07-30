f1a0cce6eaf86d78b5dc6cd73cd2dd09288e548f
jachu51 <jachu51@38ae7965-9959-4cb9-b587-4855e4c9c4a6>
2014-07-30 12:35:13 +0000

    Brakujące pliki.

---
+++ branches/gpu/MovementConstraints/Camera/Camera.cu
@@ -0,0 +1,23 @@
+/*
+ * CameraUtils.cu
+ *
+ *  Created on: Jul 30, 2014
+ *      Author: robots
+ */
+//#include <opencv2/opencv.hpp>
+
+//#include <cuda_runtime.h>
+//#include <vector_types.h>
+
+#include "CameraKernels.cu"
+
+void reprojectCameraPoints(float* cameraMatrix,
+							float* distCoeffs,
+							float* posImuMapCenter,
+							float* cameraOrigImu,
+							float* imuOrigGlobal,
+							float* segments)
+{
+
+}
+
---
+++ branches/gpu/MovementConstraints/Camera/CameraKernels.cu
@@ -0,0 +1,9 @@
+
+#ifndef CAMERA_KERNELS_CU
+#define CAMERA_KERNELS_CU
+
+__global__ void compPointReprojection(float* a){
+
+}
+
+#endif //CAMERA_KERNELS_CU
