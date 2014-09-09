637006ce672c0bee2ef0883916c58d143ea32277
jachu51 <jachu51@38ae7965-9959-4cb9-b587-4855e4c9c4a6>
2014-09-09 10:50:36 +0000

    Poprawiono błąd w ClassifierSVM. Trzeba było ustawić parametr shrinking.

--- trunk/MovementConstraints/Camera/HierClassifier/ClassifierSVM.cpp
+++ trunk/MovementConstraints/Camera/HierClassifier/ClassifierSVM.cpp
@@ -38,6 +38,7 @@ void ClassifierSVM::startup(){
 	svmParams.cache_size = 32;
 	svmParams.eps = 0.001;
 	svmParams.probability = 1;
+	svmParams.shrinking = 0;
 }
 
 void ClassifierSVM::clearData(){
@@ -258,7 +259,9 @@ void ClassifierSVM::prepareProblem(	const std::vector<Entry>& entries,
 	svmProblem.y = dataLabels;
 	svmProblem.x = labData;
 
-	if(svm_check_parameter(&svmProblem, &svmParams) != NULL){
+	const char* checkRes= svm_check_parameter(&svmProblem, &svmParams);
+	if(checkRes != NULL){
+		cout << checkRes << endl;
 		throw "Bad svm params";
 	}
 }
@@ -350,6 +353,105 @@ ClassifierSVM::~ClassifierSVM(){
 ClassifierSVM* ClassifierSVM::copy(){
 	return new ClassifierSVM(*this);
 }
+
+/*static const unsigned int NUM_INDENTS_PER_SPACE=2;
+
+const char * getIndent( unsigned int numIndents )
+{
+	static const char * pINDENT="                                      + ";
+	static const unsigned int LENGTH=strlen( pINDENT );
+	unsigned int n=numIndents*NUM_INDENTS_PER_SPACE;
+	if ( n > LENGTH ) n = LENGTH;
+
+	return &pINDENT[ LENGTH-n ];
+}
+
+// same as getIndent but no "+" at the end
+const char * getIndentAlt( unsigned int numIndents )
+{
+	static const char * pINDENT="                                        ";
+	static const unsigned int LENGTH=strlen( pINDENT );
+	unsigned int n=numIndents*NUM_INDENTS_PER_SPACE;
+	if ( n > LENGTH ) n = LENGTH;
+
+	return &pINDENT[ LENGTH-n ];
+}
+
+int dump_attribs_to_stdout(TiXmlElement* pElement, unsigned int indent)
+{
+	if ( !pElement ) return 0;
+
+	TiXmlAttribute* pAttrib=pElement->FirstAttribute();
+	int i=0;
+	int ival;
+	double dval;
+	const char* pIndent=getIndent(indent);
+	printf("\n");
+	while (pAttrib)
+	{
+		printf( "%s%s: value=[%s]", pIndent, pAttrib->Name(), pAttrib->Value());
+
+		if (pAttrib->QueryIntValue(&ival)==TIXML_SUCCESS)    printf( " int=%d", ival);
+		if (pAttrib->QueryDoubleValue(&dval)==TIXML_SUCCESS) printf( " d=%1.1f", dval);
+		printf( "\n" );
+		i++;
+		pAttrib=pAttrib->Next();
+	}
+	return i;
+}
+
+void dump_to_stdout( TiXmlNode* pParent, unsigned int indent = 0 )
+{
+	if ( !pParent ) return;
+
+	TiXmlNode* pChild;
+	TiXmlText* pText;
+	int t = pParent->Type();
+	printf( "%s", getIndent(indent));
+	int num;
+
+	switch ( t )
+	{
+	case TiXmlNode::TINYXML_DOCUMENT:
+		printf( "Document" );
+		break;
+
+	case TiXmlNode::TINYXML_ELEMENT:
+		printf( "Element [%s]", pParent->Value() );
+		num=dump_attribs_to_stdout(pParent->ToElement(), indent+1);
+		switch(num)
+		{
+			case 0:  printf( " (No attributes)"); break;
+			case 1:  printf( "%s1 attribute", getIndentAlt(indent)); break;
+			default: printf( "%s%d attributes", getIndentAlt(indent), num); break;
+		}
+		break;
+
+	case TiXmlNode::TINYXML_COMMENT:
+		printf( "Comment: [%s]", pParent->Value());
+		break;
+
+	case TiXmlNode::TINYXML_UNKNOWN:
+		printf( "Unknown" );
+		break;
+
+	case TiXmlNode::TINYXML_TEXT:
+		pText = pParent->ToText();
+		printf( "Text: [%s]", pText->Value() );
+		break;
+
+	case TiXmlNode::TINYXML_DECLARATION:
+		printf( "Declaration" );
+		break;
+	default:
+		break;
+	}
+	printf( "\n" );
+	for ( pChild = pParent->FirstChild(); pChild != 0; pChild = pChild->NextSibling())
+	{
+		dump_to_stdout( pChild, indent+1 );
+	}
+}*/
 
 void ClassifierSVM::loadSettings(TiXmlElement* settings){
 	string tmp;
